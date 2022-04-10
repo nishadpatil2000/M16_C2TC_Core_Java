691c706779ac06a0e1568d09c1b3c785f08908da
himus <himus@PRSNL_HimanshuP>
2022-04-10 23:26:56 +0530

    first commit

---
+++ Day2 intro_to_java/src/com/cg/git/Intro_Example1.java
@@ -0,0 +1,21 @@
+package com.cg.git;
+
+public class Intro_Example1 {
+
+	public static void main(String[] args) {
+		int a=15;
+		float b=10.5f;
+		double c=14.7;
+		char d='x';
+		String str="Nishad Patil";
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(str);
+
+		
+
+	}
+
+}
