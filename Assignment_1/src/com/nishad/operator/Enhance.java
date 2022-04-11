d36285f7eae88ff67675f0bdcf32d4870479d514
himus <himus@PRSNL_HimanshuP>
2022-04-11 15:35:45 +0530

    first commit

---
+++ Assignment_1/src/com/nishad/operator/Enhance.java
@@ -0,0 +1,14 @@
+package com.nishad.operator;
+class Enhance{  
+
+public static void main(String args[]){  
+     
+   int arr[]={8,0,4,2,0,2,2};  
+  
+   for(int i:arr){  
+     System.out.println(i);  
+   }  
+ }   
+}  
+
+
