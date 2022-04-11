5704b1ad73a0e829da511a9b4ce804bfe64b41e0
himus <himus@PRSNL_HimanshuP>
2022-04-11 11:57:19 +0530

    first commit

---
+++ Assignment_1/src/com/nishad/operator/Do_while_Example.java
@@ -0,0 +1,15 @@
+package com.nishad.operator;
+
+public class Do_while_Example {
+    
+	public static void main(String[] args) {    
+	    int i=1;    
+	    do{    
+	        System.out.println("Nishad");    
+	    i++;    
+	    }while(i<=7);    
+	}    
+	}   
+
+
+
---
+++ Assignment_1/src/com/nishad/operator/pattern_for_loop.java
@@ -0,0 +1,18 @@
+package com.nishad.operator;
+
+
+public class pattern_for_loop {
+	
+		public static void main(String args[])
+		{
+		int j,i,n=5;
+		for(i=1;i<=n;i++)
+		{
+		for(j=1;j<=i;j++)
+		System.out.print("@");
+		System.out.println("");
+
+		}
+		}
+		
+}
