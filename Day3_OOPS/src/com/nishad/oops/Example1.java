9376bfbb90d712500705be0eb11d4e48b0400f65
himus <himus@PRSNL_HimanshuP>
2022-04-12 15:05:47 +0530

    first commit

---
+++ Day3_OOPS/src/com/nishad/oops/Example1.java
@@ -0,0 +1,13 @@
+package com.nishad.oops;
+
+public class Example1 {
+	Example1()
+	{
+		System.out.println("Welcome to Java Full Stack Program with capgemini");
+	}
+
+	public static void main(String[] args) {
+       Example1 e=new Example1();
+	}
+
+}
---
+++ Day3_OOPS/src/com/nishad/oops/car.java
@@ -0,0 +1,23 @@
+package com.nishad.oops;
+
+public class car {
+	String name="BMW";
+	void print()
+	{
+		System.out.println("This is "+name);
+	}
+
+	public static void main(String[] args)
+	{
+	car obj1=new car();
+	//car obj=2new car();
+	//car obj3=new car();
+	obj1.print();
+	//obj2.print();
+	//obj3.print();
+
+
+
+	}
+
+}
